96560c1dadd00efe4e53ede621c36d5a463899f7
Brett Kessler <brettkessler1@gmail.com>
2018-05-02 17:56:56 -0500

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,15 +14,14 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
-	socket.emit('newMessage', {
-		from: 'Missy',
-		text: 'Nothing',
-		createdAt: 5934
+	socket.on('createMessage', (message) => {
+		console.log('createMessage', message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
 	});
-
-	socket.on('createMessage', (newMessage) => {
-		console.log('createMessage', newMessage)
-	})
 
 	socket.on('disconnect', (socket) => {
 		console.log('Disconnected from server');
