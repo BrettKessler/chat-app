b8a3c0a0e17aaabc29aa5567cf07d89993577f36
Brett Kessler <brettkessler1@gmail.com>
2018-05-12 14:21:01 -0500

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+	// Selectors
+	var messages = $('#messages');
+	var newMessage = messages.children('li:last-child');
+	// Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	} 
+}
+
 socket.on('connect', function () {
 	console.log('Connected to server');
 });
@@ -18,6 +34,8 @@ socket.on('newMessage', function(message) {
 	});
 
 	$('#messages').append(html);
+
+	scrollToBottom()
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -28,8 +46,10 @@ socket.on('newLocationMessage', function (message) {
 		url: message.url,
 		createdAt: formattedTime
 	});
-	
+
 	$('#messages').append(html);
+
+	scrollToBottom();
 });
 
 $('#message-form').on('submit', function(e) {
@@ -52,7 +72,7 @@ var locationButton = $('#send-location');
 locationButton.on('click', function () {
 	if(!navigator.geolocation){
 		return alert('Geolocation not supported by your browser');
-	} 
+	};
 
 	locationButton.attr('disabled', 'disabled').text('Sending location...')
 
